8676688828ef1836f80885e436d9811a326cbe1b
DOHMATOB Elvis <elvis.dohmatob@inria.fr>
2018-02-05 16:43:21 +0100

    wip
    mend

--- examples/nilearn/fmri_dict_learning.py
+++ examples/nilearn/fmri_dict_learning.py
@@ -1,7 +1,9 @@
 from functools import partial
 import numpy as np
+from sklearn.base import clone
 from sklearn.externals.joblib import Memory
 from sklearn.utils import gen_batches
+from sklearn.utils.validation import check_is_fitted
 from sklearn.decomposition import MiniBatchDictionaryLearning
 from nilearn._utils.compat import _basestring
 from nilearn.decomposition import (DictLearning as
@@ -26,7 +28,7 @@ class ProximalfMRIMiniBatchDictionaryLearning(fMRIMiniBatchDictionaryLearning):
                  dict_penalty_model=L11_PENALTY, positive=True, batch_size=20,
                  n_epochs=1, reduction_ratio=1., dict_init=None, alpha=1.,
                  dict_alpha=1., fit_algorithm="lars", rescale_atoms=False,
-                 verbose=0):
+                 callback=None, learning_curve_nticks=10, verbose=0):
         fMRIMiniBatchDictionaryLearning.__init__(
             self, n_components=n_components, random_state=random_state,
             mask=mask, smoothing_fwhm=smoothing_fwhm, standardize=standardize,
@@ -41,6 +43,22 @@ class ProximalfMRIMiniBatchDictionaryLearning(fMRIMiniBatchDictionaryLearning):
         self.positive = positive
         self.fit_algorithm = fit_algorithm
         self.rescale_atoms = rescale_atoms
+        self.callback = callback
+        self.learning_curve_nticks = learning_curve_nticks
+
+    def _load_data(self, data, confounds=None):
+        if isinstance(data[0], _basestring):
+            if data[0].endswith(".npy"):
+                return load_imgs(data)
+            else:
+                return mask_and_reduce(
+                    self.masker_, data, confounds,  n_jobs=self.n_jobs,
+                    reduction_ratio=self.reduction_ratio,
+                    n_components=self.n_components,
+                    random_state=self.random_state,
+                    memory_level=max(0, self.memory_level - 1))
+        else:
+            return np.asarray(data)
 
     def fit(self, imgs, y=None, confounds=None):
         BaseDecomposition.fit(self, imgs)
@@ -58,6 +76,7 @@ class ProximalfMRIMiniBatchDictionaryLearning(fMRIMiniBatchDictionaryLearning):
         data: ndarray,
             Shape (n_samples, n_features)
         """
+        # misc
         updater = partial(_general_update_dict, reg=self.dict_alpha,
                           penalty_model=self.dict_penalty_model,
                           positive=self.positive)
@@ -66,29 +85,31 @@ class ProximalfMRIMiniBatchDictionaryLearning(fMRIMiniBatchDictionaryLearning):
             verbose=self.verbose, updater=updater, ensure_nonzero=True,
             fit_algorithm=self.fit_algorithm, dict_init=self.dict_init,
             n_iter=1)
+        n_samples = len(data)
+        batch_size = min(n_samples, self.batch_size)
+        batches = list(gen_batches(len(data), batch_size))
+        n_iter = len(batches)
+        cb_freq = max(1, n_iter // self.learning_curve_nticks)
 
-        batch_size = min(len(data), self.batch_size)
-        n_iter = int(np.ceil(len(data) // float(batch_size)))
+        # loop over data and incrementally fit the model
         for epoch in range(self.n_epochs):
-            self._log('Epoch %02i/%02i' % (epoch + 1, self.n_epochs))
             cnt = 0
-            for batch in gen_batches(len(data), batch_size):
+            for batch in batches:
+                # update model on incoming mini batch of data
                 cnt += 1
-                self._log('batch %02i/%02i' % (cnt, n_iter))
-                data_batch = data[batch]
-                if isinstance(data_batch[0], _basestring):
-                    if data_batch[0].endswith(".npy"):
-                        data_batch = load_imgs(data_batch)
-                    else:
-                        data_batch = mask_and_reduce(
-                            self.masker_, data_batch, confounds,
-                            reduction_ratio=self.reduction_ratio,
-                            n_components=self.n_components, n_jobs=self.n_jobs,
-                            random_state=self.random_state,
-                            memory_level=max(0, self.memory_level - 1))
+                self._log('Epoch %02i/%02i, batch %02i/%02i' % (
+                    epoch + 1, self.n_epochs, cnt, n_iter))
+                data_batch = self._load_data(data[batch])
                 self.sodl_.partial_fit(data_batch)
-        self.components_ = self.sodl_.components_
 
+                # invoke user-supplied callback
+                if self.callback is not None:
+                    if (cnt % cb_freq == 0 or
+                        cnt == n_iter - 1):
+                        self.callback(locals())
+
+        # final sip
+        self.components_ = self.sodl_.components_
         if self.rescale_atoms:
             # Unit-variance scaling
             S = np.sqrt(np.sum(self.components_ ** 2, axis=1))
@@ -101,5 +122,25 @@ class ProximalfMRIMiniBatchDictionaryLearning(fMRIMiniBatchDictionaryLearning):
             for component in self.components_:
                 if np.sum(component > 0) < np.sum(component < 0):
                     component *= -1
-
         return self
+
+    def transform(self, imgs, components=None, batch_size=None):
+        if components is None:
+            check_is_fitted(self, "sodl_")
+            sodl = self.sodl_
+        else:
+            sodl = clone(self.sodl_)
+            sodl.components_ = components
+        if isinstance(imgs, _basestring):
+            imgs = [imgs]
+        n_imgs = len(imgs)
+        n_components = len(sodl.components_)
+        codes = np.ndarray((n_imgs, n_components),
+                           dtype=sodl.components_.dtype)
+        if batch_size is None:
+            batch_size = self.batch_size
+        batch_size = min(n_imgs, batch_size)
+        for batch in gen_batches(len(imgs), batch_size):
+            data_batch = self._load_data(imgs[batch])
+            codes[batch] = sodl.transform(data_batch)
+        return codes
--- examples/nilearn/plot_brain_dict_learning.py
+++ examples/nilearn/plot_brain_dict_learning.py
@@ -1,5 +1,8 @@
 import os
 import glob
+from functools import partial
+import numpy as np
+from sklearn.model_selection import train_test_split
 from nilearn.image.image import check_niimg
 from nilearn.masking import unmask
 from nilearn_prox_operators import ProximalOperator
@@ -8,14 +11,16 @@ import sys
 sys.path.append(os.path.join(os.environ["HOME"],
                              "CODE/FORKED/elvis_dohmatob/rsfmri2tfmri"))
 from config import hcp_distro, data_dir, root
+from feature_extraction import _enet_coder
+from utils import score_multioutput
 
 random_state = 42
 n_components = 40
 bcd_n_iter = 1
-n_epochs = 1
-dict_alpha = 100.
+n_epochs = 2
+dict_alpha = 1.
 batch_size = 30
-dataset = "IBC bold"
+dataset = "IBC zmaps"
 
 if dataset == "HCP":
     from datasets import fetch_hcp_task
@@ -24,17 +29,18 @@ if dataset == "HCP":
     X = zmaps[zmaps.contrast_name == "STORY-MATH"].groupby( 
        "subject_id")["zmap"].apply(sum)
 elif dataset == "IBC zmaps":
-    X = sorted(glob.glob(os.path.join(root,
-                                      "storage/store/data/ibc",
-                                      "derivatives/sub-*/ses-*",
-                                      "res_stats_hcp_*_ffx",
-                                      "stat_maps/*.nii.gz")))
-    mask_img = os.path.join("/home/elvis/mnt/32-bit-system/home/elvis/drago",
-    "storage/store/data/ibc/derivatives/group/gm_mask.nii.gz")
+    zmap_file_pattern = os.path.join(root,
+                                     "storage/store/data/ibc",
+                                     "derivatives/sub-*/ses-*",
+                                     "res_stats_hcp_*_ffx",
+                                     "stat_maps/*.nii.gz")
+    X = sorted(glob.glob(zmap_file_pattern))
+    mask_img = os.path.join(root, "storage/store/data/ibc/derivatives/group",
+                            "mask.nii.gz")
 elif dataset == "IBC bold":
     from datasets import load_imgs
     mask_img = os.path.join(root, "storage/store/data/ibc/derivatives/group",
-                            "movie_mask.nii.gz")
+                            "mask.nii.gz")
     X = glob.glob(os.path.join(root, "storage/store/data/ibc/derivatives",
                                "sub-*/ses-05/func/wrdcsub-*_bold.npy"))
     X = [img for Xs in load_imgs(X) for img in Xs]
@@ -45,25 +51,43 @@ batch_size = min(batch_size, len(X))
 mask_img = check_niimg(mask_img)
 n_voxels = mask_img.get_data().sum()
 
+X_train, X_test = train_test_split(X, train_size=.75)
+
+artefacts = dict(components=[], codes=[], r2=[], pearsonr=[])
+
+
+def callback(env):
+    model = env["self"]
+    components = model.sodl_.components_.copy()
+    codes = model.transform(X_test)
+    artefacts["components"].append(components)
+    artefacts["codes"].append(codes)
+    X_true = model._load_data(X_test)
+    X_pred = np.dot(codes, components)
+    for scorer in ["r2", "pearsonr"]:
+        scores = score_multioutput(X_true.T, X_pred.T, scorer=scorer)
+        artefacts[scorer].append(scores)
+
+
 prox = ProximalOperator(which="graph-net", affine=mask_img.affine, fwhm=2,
                         mask=mask_img.get_data().astype(bool), l1_ratio=.1,
                         kernel="gaussian", radius=10.)
 model = ProximalfMRIMiniBatchDictionaryLearning(
-    n_components=n_components, random_state=random_state, fit_algorithm="ridge",
-    dict_penalty_model=-1, mask=mask_img, n_epochs=1, batch_size=batch_size,
-    dict_alpha=dict_alpha, verbose=2)
+    n_components=n_components, random_state=random_state,
+    fit_algorithm=partial(_enet_coder, l1_ratio=0., alpha=1.),
+    dict_penalty_model=-2, mask=mask_img, n_epochs=n_epochs, callback=callback,
+    batch_size=batch_size, dict_alpha=dict_alpha, verbose=1)
 model.fit(X)
 
 import matplotlib.pyplot as plt
 from nilearn.plotting import plot_stat_map
 
-for c in range(0, n_components, 2):
+for c in range(0, n_components, 4):
     if c == 3:
         continue
     plot_stat_map(unmask(model.components_[c], mask_img), display_mode="ortho",
                   colorbar=False, black_bg=True)
-    out_file = os.path.join("/home/elvis/CODE/FORKED/coordescendant/paper",
-                            "figs/%s_comp%02i.png" % (dataset, c))
+    out_file = "%s_comp%02i.png" % (dataset.replace(" ", "_"), c)
     plt.savefig(out_file, dpi=200, bbox_inches="tight", pad_inches=0)
     os.system("mogrify -trim %s" % out_file)
     print(out_file)
